20f6c03fb08e788fbb36e0ebdb1794f58c9c20db
komcat <komgritclick@gmail.com>
2025-04-27 19:50:28 -0700

    Add project files.

---
+++ cppuaa/cppuaa.h
@@ -0,0 +1,15 @@
+﻿// cppuaa.h
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+// ImGui and SDL includes
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+
+// Include SDL2
+#include <SDL.h>
+#include <SDL_opengl.h>
